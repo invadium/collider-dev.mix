f6afa1d80fd4c90fa4e790f0ad025f28f5249ed0
Igor Khotin <khotin@gmx.com>
2026-06-13 03:05:45 +0300

    [+] debug console open/close traps and env/pauseRootLab flag

--- console.mod/lib/control.js
+++ console.mod/lib/control.js
@@ -1,9 +1,13 @@
 // show the console
 function open() {
     __$.disableOthers()
+    if (__$.env.pauseRootLab) {
+        $.pauseLab()
+    }
     __$.show()
     lab.hud.captureFocus(lab.hud.console)
     lab.hud.console.show()
+    signal('open')
 }
 
 // hide the console
@@ -12,5 +16,9 @@ function close() {
     lab.hud.releaseFocus(lab.hud.console)
     lab.hud.console.resetHistory()
     lab.hud.console.hide()
+    if (__$.env.pauseRootLab) {
+        $.resumeLab()
+    }
     __$.hide()
+    signal('close')
 }
